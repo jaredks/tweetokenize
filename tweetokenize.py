8755770eeeadc397e7830f25d98fd30bf01f7ebc
Jared <jaredks@gmail.com>
2013-05-11 03:54:36 -0400

    initial

---
+++ tweetokenize.py
@@ -0,0 +1,282 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright: (c) 2013, Jared Suttles. All rights reserved.
+# License: See LICENSE for details.
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+"""
+Tokenization and pre-processing for social media data used to train classifiers.
+Focused on classification of sentiment, emotion, or mood.
+
+Intended as glue between Python wrappers for Twitter API and the Natural 
+Language Toolkit (NLTK), but probably applicable to tokenizing any short 
+messages of the social networking variety.
+
+In many cases, reducing feature-set complexity can increase performance of 
+classifiers trained for detecting sentiment. The available settings are based 
+on commonly modified and normalized features in classification research using 
+content from Twitter.
+"""
+
+import re
+from os import path
+try:
+    from itertools import imap
+except ImportError:
+    imap = map
+from htmlentitydefs import name2codepoint
+
+class Tokenizer(object):
+    """
+    Can be used to tokenize a string representation of a message, adjusting 
+    features based on the given configuration details, to enable further 
+    processing in feature extraction and training stages.
+    
+    An example usage::
+    
+      >>> from tweetokenize import Tokenizer
+      >>> gettokens = Tokenizer(usernames='USER', urls='')
+      >>> gettokens.tokenize('@justinbeiber yo man!love you#inlove#wantyou in a totally straight way #brotime <3:p:D www.justinbeiber.com')
+      [u'USER', u'yo', u'man', u'!', u'love', u'you', u'#inlove', u'#wantyou', u'in', u'a', u'totally', u'straight', u'way', u'#brotime', u'<3', u':p', u':D']
+    """
+    class TokenizerException(BaseException): pass
+    html_entities = {k:unichr(v) for k,v in name2codepoint.items()}
+    __default_args = None
+    
+    # Regular expressions
+    usernames_re = re.compile(r"@\w{1,15}")
+    _topleveldomains = 'museum|travel|aero|arpa|asia|coop|info|jobs|mobi|name|post|biz|cat|com|edu|gov|int|mil|net|org|pro|tel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cu|cv|cw|cx|cy|cz|de|dj|dk|dm|do|dz|ec|ee|eg|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|sk|sl|sm|sn|so|sr|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|za|zm|zw'
+    urls_re = re.compile(r"(?:(?:https?\://[A-Za-z0-9\.]+)|(?:(?:www\.)?[A-Za-z0-9]+\.(?:%s)))(?:\/\S+)?(?=\s+|$)" % _topleveldomains)
+    hashtags_re = re.compile(r"#\w+[\w'-]*\w+")
+    ellipsis_re = re.compile(r"\.\.+")
+    word_re = re.compile(r"(?:[a-zA-Z0-9]+['-]?[a-zA-Z]+[a-zA-Z0-9]*)|(?:[a-zA-Z0-9]*[a-zA-Z]+['-]?[a-zA-Z0-9]+)")
+    times_re = re.compile(r"\d{1,2}:\d{2}(?::\d{2})?\s*(?:AM|PM|am|pm)?")
+    phonenumbers_re = re.compile(r"(?:\+?[01][\-\s\.]*)?(?:\(?\d{3}[\-\s\.\)]*)?\d{3}[\-\s\.]*\d{4}(?:\s*x\s*\d+)?(?=\s+|$)")
+    _number = r"(?:[+-]?\$?\d+(?:\.\d+)?(?:[eE]-?\d+)?%?)(?![A-Za-z])"
+    numbers_re = re.compile(r"%(num)s(?:\s*/\s*%(num)s)?" % {'num': _number}) # deals with fractions
+    other_re = r"(?:[^#\s\.]|\.(?!\.))+"
+    _token_regexs = ('usernames', 'urls', 'hashtags', 'times', 'phonenumbers', 'numbers')
+    tokenize_re = re.compile(ur"|".join(imap(lambda x: getattr(x, 'pattern', x),
+        [locals()['%s_re' % regex] for regex in _token_regexs] + [word_re, ellipsis_re, other_re]))); del regex
+    html_entities_re = re.compile(r"&#?\w+;")
+    repeating_re = re.compile(r"([a-zA-Z])\1\1+")
+    _doublequotes = ((u'“',u'”'),(u'"',u'"'),(u'‘',u'’'),(u'＂',u'＂'))
+    punctuation = (u'!$%()*+,-/:;<=>?[\\]^_.`{|}~\'' + u''.join(c for t in _doublequotes for c in t))
+    quotes_re = re.compile(ur"|".join(ur'(%s.*?%s)' % t for t in _doublequotes) + ur'|\s(\'.*?\')\s')
+    #emoji_re = re.compile(ur'[\U0001f300-\U0001f5ff....
+    
+    def __init__(self, lowercase=True, allcapskeep=True, normalize=3, usernames='USERNAME', urls='URL', hashtags=False, phonenumbers='PHONENUMBER', times='TIME', numbers='NUMBER', ignorequotes=False):
+        """
+        Constructs a new Tokenizer. Can specify custom settings for various 
+        feature normalizations.
+        
+        Any features with replacement tokens can be removed from the message by 
+        setting the token to the empty string (C{""}), C{"DELETE"}, or 
+        C{"REMOVE"}.
+        
+        @type lowercase: C{bool}
+        @param lowercase: If C{True}, lowercases words, excluding those with all 
+            letters capitalized.
+        
+        @type allcapskeep: C{bool}
+        @param allcapskeep: If C{True}, maintains capitalization for words with 
+            all letters in capitals. Otherwise, capitalization for such words 
+            is dependent on C{lowercase}.
+        
+        @type normalize: C{int}
+        @param normalize: The number of repeating letters when normalizing 
+            arbitrary letter elongations.
+            
+            Example::
+                Heyyyyyy i lovvvvvvve youuuuuuuuu <3
+            
+            Becomes::
+                Heyyy i lovvve youuu <3
+            
+            Not sure why you would want to change this (maybe just for fun?? :P)
+        
+        @param usernames: Serves as the replacement token for anything that 
+            parses as a Twitter username, ie. C{@rayj}. Setting this to 
+            C{False} means no usernames will be changed.
+        
+        @param urls: Serves as the replacement token for anything that 
+            parses as a URL, ie. C{bit.ly} or C{http://example.com}. Setting 
+            this to C{False} means no URLs will be changed.
+        
+        @param hashtags: Serves as the replacement token for anything that 
+            parses as a Twitter hashtag, ie. C{#ihititfirst} or 
+            C{#onedirection}. Setting this to C{False} means no hashtags will 
+            be changed.
+        
+        @param phonenumbers: Replacement token for phone numbers.
+        
+        @param times: Replacement token for times.
+        
+        @param numbers: Replacement token for any other kinds of numbers.
+        
+        @type ignorequotes: C{bool}
+        @param ignorequotes: If C{True}, will remove various types of quotes and 
+            the contents within.
+        """
+        if Tokenizer.__default_args is None:
+            Tokenizer.__default_args = locals().keys()
+        for keyword, value in locals().items():
+            setattr(self, keyword, value)
+        self.emoticonsfromfile(path.dirname(path.realpath(__file__))+'/lexicons/emoticons.txt')
+    
+    def __call__(self, iterable):
+        """
+        Iterator for the tokenization of given messages.
+        
+        @rtype: C{list} of C{str}
+        @return: Iterator of lists representing message tokenizations.
+        
+        @param iterable: Object capable of iteration, providing strings for 
+            tokenization.
+        """
+        for msg in iterable:
+            yield self.tokenize(msg)
+    
+    def update(self, **kwargs):
+        """
+        Adjust any settings of the Tokenizer.
+        
+          >>> gettokens.lowercase
+          True
+          >>> gettokens.phonenumbers
+          'PHONENUMBER'
+          >>> gettokens.update(phonenumbers='NUMBER', lowercase=False)
+          >>> gettokens.lowercase
+          False
+          >>> gettokens.phonenumbers
+          'NUMBER'
+        """
+        for keyword in Tokenizer.__default_args:
+            if keyword in kwargs:
+                setattr(self, keyword, kwargs[keyword])
+    
+    def _converthtmlentities(self, msg):
+        def replace_entities(s):
+            s = s.group(0)[1:-1] # remove & and ;
+            if s[0] == '#':
+                try:
+                    return unichr(int(s[2:], 16) if s[1] in ('x','X') else int(s[1:]))
+                except ValueError:
+                    return '&#' + s + ';'
+            else:
+                try:
+                    return Tokenizer.html_entities[s]
+                except KeyError:
+                    return '&' + s + ';'
+        return Tokenizer.html_entities_re.sub(replace_entities, msg)
+    
+    def _replacetokens(self, msg):
+        tokens = []; deletion_tokens = ('','REMOVE','remove','DELETE','delete')
+        for word in msg:
+            matching = Tokenizer.word_re.match(word) # 1st check if normal word
+            if matching and len(matching.group(0)) == len(word):
+                tokens.append(self._cleanword(word))
+                continue # don't check rest of conditions
+            for token in Tokenizer._token_regexs: # id & possibly replace tokens
+                regex = getattr(Tokenizer, '%s_re' % token)
+                replacementtoken = getattr(self, token)
+                if regex.match(word):
+                    if replacementtoken: # decide if we change it
+                        word = self._unicode(str(replacementtoken))
+                    if replacementtoken not in deletion_tokens:
+                        tokens.append(word)
+                    break
+            else: # we didn't find a match for any token so far...
+                if Tokenizer.ellipsis_re.match(word):
+                    tokens.append(u"...")
+                else: # split into tokens based on emoticons or punctuation
+                    tokens.extend(self._separate_emoticons_punctuation(word))
+        return tokens
+    
+    def _separate_emoticons_punctuation(self, word):
+        newwords=[]; wordbefore=u""; i=0
+        def possibly_append_and_reset(w):
+            if w:
+                newwords.append(self._cleanword(w))
+            return u""
+        while i < len(word):
+            # greedily check for emoticons in this word
+            for l in range(self._maxlenemo, 0, -1):
+                if word[i:i+l] in self._emoticons or self._isemoji(word[i:i+l]):
+                    wordbefore = possibly_append_and_reset(wordbefore)
+                    newwords.append(word[i:i+l])
+                    i+=l
+                    break
+            else: # its safe to break up any punctuation not part of emoticons
+                if word[i] in Tokenizer.punctuation:
+                    wordbefore = possibly_append_and_reset(wordbefore)
+                    newwords.append(word[i])
+                else:
+                    wordbefore += word[i]
+                i+=1
+        # possible ending of the word which wasn't emoticon or punctuation
+        possibly_append_and_reset(wordbefore)
+        return newwords
+    
+    def _isemoji(self, s):
+        emoji_ranges = ((u'🌀', u'🗿'), (u'😀', u'🙏'), (u'🚀', u'🛅'), (u'☀', u'⛿'), (u'🅰', u'🆚'))
+        emoji_flags =  set([u'🇯🇵', u'🇰🇷', u'🇩🇪', u'🇨🇳', u'🇺🇸', u'🇫🇷', u'🇪🇸', u'🇮🇹', u'🇷🇺', u'🇬🇧'])
+        check_emoji = lambda given: len(given) == len(u'💩') and any(e[0] <= given <= e[1] for e in emoji_ranges)
+        return check_emoji(s) or s in emoji_flags
+    
+    def _cleanword(self, word):
+        if self.normalize: # replace characters with >=3 alphabetic repeating
+            word = Tokenizer.repeating_re.sub(r"\1"*self.normalize, word)
+        if self.lowercase and (not self.allcapskeep or not word.isupper()):
+            return word.lower()
+        return word
+    
+    def _unicode(self, word):
+        if isinstance(word, unicode):
+            return word
+        return unicode(word, encoding='utf-8')
+    
+    def tokenize(self, message):
+        """
+        Tokenize the given string into a list of strings representing the 
+        constituent words of the message.
+        
+        @rtype: C{list} of C{str}
+        @return: The tokenization of the message.
+        
+        @type message: C{str}
+        @param message: The string representation of the message.
+        """
+        if not isinstance(message, basestring):
+            raise Tokenizer.TokenizerException('cannot tokenize non-string, %s' % repr(message.__class__.__name__))
+        message = self._unicode(message)
+        message = self._converthtmlentities(message)
+        if self.ignorequotes:
+            message = Tokenizer.quotes_re.sub(" ", message)
+        message = Tokenizer.tokenize_re.findall(message)
+        return self._replacetokens(message)
+    
+    def emoticons(self, iterable):
+        """
+        Consumes an iterable of emoticons that the tokenizer will tokenize on. 
+        Allows for user-specified set of emoticons to be recognized.
+        
+        @param iterable: Object capable of iteration, providing emoticon 
+            strings.
+        """
+        self._emoticons = set(imap(self._unicode, iterable))
+        self._maxlenemo = max(len(max(self._emoticons, key=lambda x: len(x))), len(u'🇨🇳'), len(u'💋'))
+    
+    def emoticonsfromfile(self, filename):
+        """
+        Import emoticon tokens delimited by newlines from a given file into a 
+        new set that the tokenizer will tokenize on. Strips trailing whitespace 
+        and skips blank lines.
+        
+        @type filename: C{str}
+        @param filename: Path to the file containing emoticons delimited by 
+            new lines.
+        """
+        with open(filename, "r") as f:
+            self.emoticons(self._unicode(l.rstrip()) for l in f if l.strip())
---
+++ unittests_tweetokenize.py
@@ -0,0 +1,253 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright: (c) 2013, Jared Suttles. All rights reserved.
+# License: See LICENSE for details.
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+import unittest
+from tweetokenize import Tokenizer
+
+class TokenizeTests(unittest.TestCase):
+    def setUp(self):
+        self.tokenizer = Tokenizer(lowercase=True)
+    
+    def test_general_1(self):
+        self.tokenizer.normalize = 2
+        msg = ('omg wow &#x3c; &#x26; &#x3e; &#62;.&#60; &gt;.&lt; :):)'
+        'i CANT believe thatttt haha lol!!1')
+        tks = ['omg', 'wow', '<', '&', '>', '>.<', '>.<', ':)', ':)',
+        'i', 'CANT', 'believe', 'thatt', 'haha', 'lol', '!', '!', '1']
+        self.assertEqual(self.tokenizer.tokenize(msg), tks)
+    
+    def test_general_2(self):
+        msg = "i'm wanting to jump up and down but wouldn't if i couldn't.."
+        tks = [u"i'm", u'wanting', u'to', u'jump', u'up', u'and', u'down',
+        u'but', u"wouldn't", u'if', u'i', u"couldn't", u'...']
+        self.assertEqual(self.tokenizer.tokenize(msg), tks)
+    
+    def test_urls_1(self):
+        msg = ("hey bro chec'k out http://shitstorm.com its fucking sick")
+        tks = ['hey', 'bro', "chec'k", 'out', 'URL', 'its', 'fucking', 'sick']
+        self.assertEqual(self.tokenizer.tokenize(msg), tks)
+    
+    def test_urls_2(self):
+        msg = ('also see this crazy stuff https://shitstorm.com')
+        tks = ['also', 'see', 'this', 'crazy', 'stuff', 'URL']
+        self.assertEqual(self.tokenizer.tokenize(msg), tks)
+    
+    def test_urls_3(self):
+        msg = 'hiiiii rayj.com/ihititfirst and other google.com http://hobo.net'
+        tks = [u'hiii', u'URL', u'and', u'other', u'URL', u'URL']
+        self.assertEqual(self.tokenizer.tokenize(msg), tks)
+    
+    def test_usernames_1(self):
+        msg = ('@justinbeiber yo man!! ! i love you in a totally '
+        'straight way <3:p:D')
+        tks = [u'USERNAME', u'yo', u'man', u'!', u'!', u'!',
+        u'i', u'love', u'you', u'in', u'a', u'totally', u'straight', u'way',
+        u'<3', u':p', u':D']
+        self.assertEqual(self.tokenizer.tokenize(msg), tks)
+    
+    def test_usernames_2(self):
+        msg = '@heyheymango: what did you SAYYY??? or did you just..  NotHING?'
+        tks = [u'USERNAME', u':', u'what', u'did', u'you', u'SAYYY', u'?',
+        u'?', u'?', u'or', u'did', u'you', u'just', u'...', u'nothing', u'?']
+        self.assertEqual(self.tokenizer.tokenize(msg), tks)
+    
+    def test_numbers_1(self):
+        self.tokenizer.numbers = None
+        msg = ('i have this much money -2.42 in my bank acct.,friend! but you '
+        'have mucho +88e44 and its about 1000% more than $400.')
+        tks = [u'i', u'have', u'this', u'much', u'money', u'-2.42', u'in',
+        u'my', u'bank', u'acct', u'.', u',', u'friend', u'!', u'but', u'you',
+        u'have', u'mucho', u'+88e44', u'and', u'its', u'about', u'1000%',
+        u'more', u'than', u'$400', u'.']
+        self.assertEqual(self.tokenizer.tokenize(msg), tks)
+    
+    def test_numbers_2(self):
+        msg = ('i have this much money -2.42 in my bank acct.,friend! but you '
+        'have mucho +88e44 and its about 1000% more than $400.')
+        tks = [u'i', u'have', u'this', u'much', u'money', u'NUMBER', u'in',
+        u'my', u'bank', u'acct', u'.', u',', u'friend', u'!', u'but', u'you',
+        u'have', u'mucho', u'NUMBER', u'and', u'its', u'about', u'NUMBER',
+        u'more', u'than', u'NUMBER', u'.']
+        self.assertEqual(self.tokenizer.tokenize(msg), tks)
+    
+    def test_numbers_3(self):
+        self.tokenizer.lowercase = False # keep cases the same everywhere
+        msg = ('I JUST want To Test FRACTIONZZZ 22432.41414/ 55894385e-341 also'
+        ' lowercase etc.etc.etc. hope that last part doesn\'t parse as a url '
+        'i would be kinda sad PANda!zsss..... .. . .... 4/5 5.1/4.0e0 3.14 -2')
+        tks = [u'I', u'JUST', u'want', u'To', u'Test', u'FRACTIONZZZ',
+        u'NUMBER', u'also', u'lowercase', u'etc', u'.', u'etc', u'.', u'etc',
+        u'.', u'hope', u'that', u'last', u'part', u"doesn't", u'parse', u'as',
+        u'a', u'url', u'i', u'would', u'be', u'kinda', u'sad', u'PANda', u'!',
+        u'zsss', u'...', u'...', u'.', u'...', u'NUMBER', u'NUMBER', u'NUMBER',
+        u'NUMBER']
+        self.assertEqual(self.tokenizer.tokenize(msg), tks)
+    
+    def test_time_1(self):
+        msg = 'is the time now 12:14pm? or is it like 2:42AM??'
+        tks = [u'is', u'the', u'time', u'now', u'TIME', u'?', u'or', u'is',
+        u'it', u'like', u'TIME', u'?', u'?']
+        self.assertEqual(self.tokenizer.tokenize(msg), tks)
+    
+    def test_time_2(self):
+        msg = 'new time is 2:42:09 PM!!'
+        tks = [u'new', u'time', u'is', u'TIME', u'!', u'!']
+        self.assertEqual(self.tokenizer.tokenize(msg), tks)
+    
+    def test_phonenumber_1(self):
+        msg = ('my number is 18002432242 and 241.413.5584 also 1-242-156-6724'
+        ' and (958)555-4875 or (999) 415 5542 is 422-5555 a 131-121-1441')
+        tks = [u'my', u'number', u'is', u'PHONENUMBER', u'and', u'PHONENUMBER',
+        u'also', u'PHONENUMBER', u'and', u'PHONENUMBER', u'or', u'PHONENUMBER',
+        u'is', u'PHONENUMBER', u'a', u'PHONENUMBER']
+        self.assertEqual(self.tokenizer.tokenize(msg), tks)
+    
+    def test_phonenumber_2(self):
+        msg = 'numbers with extension: (201)-340-4915 x112 or 1 800.341.1311x99'
+        tks = [u'numbers', u'with', u'extension', u':', u'PHONENUMBER', u'or',
+        u'PHONENUMBER']
+        self.assertEqual(self.tokenizer.tokenize(msg), tks)
+    
+    def test_quotes_1(self):
+        self.tokenizer.ignorequotes = True
+        msg = 'this is just a tweet with "someone said something funny" lol'
+        tks = ['this', 'is', 'just', 'a', 'tweet', 'with', 'lol']
+        self.assertEqual(self.tokenizer.tokenize(msg), tks)
+    
+    def test_quotes_2(self):
+        self.tokenizer.ignorequotes = False
+        msg = 'this is just a tweet with "someone said something funny" lol'
+        tks = ['this', 'is', 'just', 'a', 'tweet', 'with', '"', 'someone',
+        'said', 'something', 'funny', '"', 'lol']
+        self.assertEqual(self.tokenizer.tokenize(msg), tks)
+    
+    def test_quotes_3(self):
+        self.tokenizer.ignorequotes = True
+        msg = ('some stuff but he said “yea i know its crazy”other '
+        'stuff...!!! ')
+        tks = [u'some', u'stuff', u'but', u'he', u'said', u'other', u'stuff',
+        u'...', u'!', u'!', u'!']
+        self.assertEqual(self.tokenizer.tokenize(msg), tks)
+    
+    def test_quotes_4(self):
+        self.tokenizer.ignorequotes = True
+        msg = ('some stuff but he said &ldquo;yea i know its crazy&rdquo;other '
+        'stuff...!!! ')
+        tks = [u'some', u'stuff', u'but', u'he', u'said', u'other', u'stuff',
+        u'...', u'!', u'!', u'!']
+        self.assertEqual(self.tokenizer.tokenize(msg), tks)
+    
+    def test_quotes_5(self):
+        self.tokenizer.ignorequotes = False
+        msg = 'heyy buddyyyyy boy \'do you the lady\'s kitty like that??\''
+        tks = [u'heyy', u'buddyyy', u'boy', u"'", u'do', u'you', u'the',
+        u"lady's", u'kitty', u'like', u'that', u'?', u'?', u"'"]
+        self.assertEqual(self.tokenizer.tokenize(msg), tks)
+    
+    def test_hashtags_1(self):
+        msg = 'omg i love#dog#cat#food#other#things#so#fucking#much!!!11LOLOLOL'
+        tks = ['omg', 'i', 'love', '#dog', '#cat', '#food', '#other',
+        '#things', '#so', '#fucking', '#much', '!', '!', '!', '11LOLOLOL']
+        self.assertEqual(self.tokenizer.tokenize(msg), tks)
+    
+    def test_hashtags_2(self):
+        self.tokenizer.hashtags = 'HASHTAG'
+        msg = 'omg i love#dog#cat#food#other#things#so#fucking#much!!!11LOLOLOL'
+        tks = ['omg', 'i', 'love', 'HASHTAG', 'HASHTAG', 'HASHTAG',
+        'HASHTAG', 'HASHTAG', 'HASHTAG', 'HASHTAG', 'HASHTAG', '!', '!', '!',
+        '11LOLOLOL']
+        self.assertEqual(self.tokenizer.tokenize(msg), tks)
+    
+    def test_emoticons_1(self):
+        msg = 'heyyyyyy:):):(>.<<v.vwhats up man LOL T.T tomcat.tomcat :$;).!!!'
+        tks = [u'heyyy', u':)', u':)', u':(', u'>.<', u'<', u'v.v', u'whats',
+        u'up', u'man', u'LOL', u'T.T', u'tomcat', u'.', u'tomcat', u':$',
+        u';)', u'.', u'!', u'!', u'!']
+        self.assertEqual(self.tokenizer.tokenize(msg), tks)
+    
+    def test_removefeatures_1(self):
+        self.tokenizer.usernames = "" # dont' want any usernames to show
+        msg = ('hey @arnold @nickelodeon #90s#ilove90s#allthat#amandashow'
+        '@rocko http://en.wikipedia.org/wiki/The_Angry_Beavers ^.^>>><<<^.^')
+        tks = [u'hey', u'#90s', u'#ilove90s', u'#allthat', u'#amandashow',
+        u'URL', u'^.^', u'>', u'>', u'>', u'<', u'<', u'<', u'^.^']
+        self.assertEqual(self.tokenizer.tokenize(msg), tks)
+    
+    def test_removefeatures_2(self):
+        self.tokenizer.usernames = "" # dont' want any usernames to show
+        self.tokenizer.hashtags = ""  # or hashtags
+        msg = ('hey @arnold @nickelodeon #90s#ilove90s#allthat#amandashow'
+        '@rocko http://en.wikipedia.org/wiki/The_Angry_Beavers ^.^>>><<<^.^')
+        tks = [u'hey', u'URL', u'^.^', u'>', u'>', u'>', u'<', u'<', u'<',
+        u'^.^']
+        self.assertEqual(self.tokenizer.tokenize(msg), tks)
+    
+    def test_removefeatures_3(self):
+        self.tokenizer.usernames = False # keep usernames
+        self.tokenizer.urls = ""         # URLs should be removed
+        self.tokenizer.hashtags = "$$$"  # hashtags should be $$$
+        msg = ('hey @arnold @nickelodeon #90s#ilove90s#allthat#amandashow'
+        '@rocko http://en.wikipedia.org/wiki/The_Angry_Beavers ^.^>>><<<^.^')
+        tks = [u'hey', u'@arnold', u'@nickelodeon', u'$$$', u'$$$', u'$$$',
+        u'$$$', u'@rocko', u'^.^', u'>', u'>', u'>', u'<', u'<', u'<', u'^.^']
+        self.assertEqual(self.tokenizer.tokenize(msg), tks)
+    
+    def test_emoji_1(self):
+        msg = ('hey mate!:):3.....@and🇨🇳ONE+ BRO#love😘😵💚💛💜💙  '
+        '💋😂😂LOLLLL.')
+        tks = [u'hey', u'mate', u'!', u':)', u':3', u'...',
+        u'USERNAME', u'\U0001f1e8\U0001f1f3', u'ONE', u'+', u'BRO', u'#love',
+        u'\U0001f618', u'\U0001f635', u'\U0001f49a', u'\U0001f49b',
+        u'\U0001f49c', u'\U0001f499', u'\U0001f48b', u'\U0001f602',
+        u'\U0001f602', u'LOLLL', u'.']
+        self.assertEqual(self.tokenizer.tokenize(msg), tks)
+    
+    def test_emoji_2(self):
+        msg = ('hey mate!:):3.....@andONE+🇬🇧  BRO#love😘😵💚💛💜💙  '
+        '💋😂😂LOLLLL.')
+        tks = [u'hey', u'mate', u'!', u':)', u':3', u'...',
+        u'USERNAME', u'+', u'\U0001f1ec\U0001f1e7', u'BRO', u'#love', u'😘',
+        u'😵', u'\U0001f49a', u'\U0001f49b', u'\U0001f49c',
+        u'\U0001f499', u'💋', u'\U0001f602', u'\U0001f602',
+        u'LOLLL', u'.']
+        self.assertEqual(self.tokenizer.tokenize(msg), tks)
+    
+    def test_emoji_3(self):
+        msg = ('🚀=)</3O_O:$D:<:-@\xf0\x9f\x98\xb7🔥💩💅 outdated:💽 ancient:💾 '
+        '#getwiththecloud:💻 and it looks like 💭')
+        tks = [u'\U0001f680', u'=)', u'</3', u'O_O', u':$', u'D:<', u':-@',
+        u'\U0001f637', u'\U0001f525', u'\U0001f4a9', u'\U0001f485',
+        u'outdated', u':', u'\U0001f4bd', u'ancient', u':',
+        u'\U0001f4be', u'#getwiththecloud',
+        u':', u'\U0001f4bb', u'and', u'it', u'looks', u'like', u'\U0001f4ad']
+        self.assertEqual(self.tokenizer.tokenize(msg), tks)
+    
+    def test_error_1(self):
+        msg = []
+        with self.assertRaises(Tokenizer.TokenizerException):
+            self.tokenizer.tokenize(msg)
+    
+    def test_error_2(self):
+        msg = lambda x:x
+        with self.assertRaises(Tokenizer.TokenizerException):
+            self.tokenizer.tokenize(msg)
+    
+    def test_actual_tweets_1(self):
+        "Number as part of name"
+        msg = '@LoganTillman not 2pac and floyd mayweather'
+        tks = [u'USERNAME', u'not', u'2pac', u'and', u'floyd', u'mayweather']
+        self.assertEqual(self.tokenizer.tokenize(msg), tks)
+    
+    def test_actual_tweets_2(self):
+        "Colon no space in hashtag"
+        msg = '#MentionSomeoneYoureGladYouMet: @LarryWorld_Wide of course.'
+        tks = [u'#MentionSomeoneYoureGladYouMet', u':', u'USERNAME', u'of',
+        u'course', u'.']
+        self.assertEqual(self.tokenizer.tokenize(msg), tks)
+
+if __name__ == "__main__":
+    unittest.main()
